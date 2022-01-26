52a1c90b02caa068636cfd62b7aa2d2c073c450e
AshishArya_In <56192979+ashisharya65@users.noreply.github.com>
2022-01-26 08:32:45 +0000

    Create SortingFilesUsingFileNames.ps1

---
+++ SortingFilesUsingFileNames.ps1
@@ -0,0 +1,33 @@
+<#
+    Write a script to sort the files (with below mentioned filenames) in an incrementing way :
+    K1__test_process.js","K3__process.js","K2__qa_testing.js","K10__dimensions.js","K5__closed_loop_testing.js
+
+#>
+
+#################################################################################################################
+# First Method : MY WAY
+###############################
+
+$Files | ForEach-Object {[PSCustomObject]@{
+    Name = $_
+    SortingProperty = [int] ($_ | ForEach-Object {($_.ToString().Split("__")[0]) -replace "[^0-9]"}) 
+}
+} | Sort-Object -Property SortingProperty | Select-object Name
+
+#################################################################################################################
+
+# Second Method
+###############################
+
+$Files = @("K1__test_process.js","K3__process.js","K2__qa_testing.js","K10__dimensions.js","K5__closed_loop_testing.js")
+$Files | sort-object {if ($_ -match '(\d+)'){[int]$matches[1]}}
+
+#################################################################################################################
+
+# Third Method
+###############################
+
+$Files | Sort-Object { $_ -replace 'K|_.+' -as [int] }
+
+
+#################################################################################################################
